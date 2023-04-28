5600f0523dae66eea02991b3efb8d388c6225e65
ikrom97 <ikromr04@gmail.com>
2023-04-28 16:34:16 +0500

    posts image click

--- src/components/layouts/footer/footer.jsx
+++ src/components/layouts/footer/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Text from '@/components/ui/text/text';
 import Link from 'next/link';
-import MainLogo from '@/components/ui/logo/logo';
+import Logo from '@/components/ui/logo/logo';
 import style from './style.module.css';
 import Container from '@/components/ui/container/container';
 
@@ -9,7 +9,7 @@ function Footer() {
   return (
     <footer className={style.footer}>
       <Container className={style.footerContainer}>
-        <MainLogo footer />
+        <Logo footer />
 
         <Text>For feedback: <Link href="mailto:info@zafarmirzo.com">info@zafarmirzo.com</Link></Text>
       </Container>
